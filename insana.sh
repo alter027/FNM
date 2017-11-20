31db979e29546b8ce654cbbb62ff662b1a903a16
alter027 <wsjyun749@gmail.com>
2017-11-20 19:07:04 +0800

    Update insana.sh

--- insana.sh
+++ insana.sh
@@ -1,13 +1,17 @@
+#!/bin/bash
+
 sudo apt-get install -y curl
 
-cd /tmp   
 curl -O https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh  
-printf '\nyes\n\nyes\nyes\n' | bash Anaconda3-4.2.0-Linux-x86_64.sh
+
+printf '\nyes\nyes\n' | \
+bash Anaconda3-4.2.0-Linux-x86_64.sh
 
 # can use conda therefore
 source ~/.bashrc
 
-conda update spyder
+conda update -y conda
+conda update -y spyder
 pip install --upgrade pip
 
 pkgs="
